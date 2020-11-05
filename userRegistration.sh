6baea7caf48d2d8b25928180523781d259d5476d
VismayRaul <vismayraul@gmail.com>
2020-11-05 17:14:00 +0530

    first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 user registration problem
+
+read -p " First name - " First
+
+FirstName="^[A-Z]{1}[a-z]{2}[a-z]*$";
+
+if [[$First=~$FirstName]]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
