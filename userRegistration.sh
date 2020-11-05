cbf95175a8224bc357229bfeade442d35249aea4
VismayRaul <vismayraul@gmail.com>
2020-11-05 22:24:02 +0530

    enter email id

--- userRegistration.sh
+++ userRegistration.sh
@@ -21,3 +21,14 @@ then
 else
         echo "invalid"
 fi
+
+read -p "Email ID - " Email
+EmailID="^[A-za-z]{1,5}[0-9A-za-z\.\+\-\_\#\$]+\@[0-9a-z]{1,5}\.[a-z]{1,3}[\.]{0,1}[a-z]{0,3}$";
+
+if [[ $Email =~ $EmailID ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
+
