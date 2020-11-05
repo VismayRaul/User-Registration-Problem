1ef6a56d6a8380a29c9ccd0ca70f4300379e8d0e
VismayRaul <vismayraul@gmail.com>
2020-11-05 20:56:02 +0530

    entering last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,12 +3,21 @@
 user registration problem
 
 read -p " First name - " First
-
 FirstName="^[A-Z]{1}[a-z]{2}[a-z]*$";
 
-if [[$First=~$FirstName]]
+if [[ $First =~ $FirstName ]]
 then
-	echo "valid"
+        echo "valid"
 else
-	echo "invalid"
+        echo "invalid"
+fi
+
+read -p " Last name - " Last
+LastName="^[A-Z]{1}[a-z]{2}[a-z]*$";
+
+if [[ $Last =~ $LastName ]]
+then
+        echo "valid"
+else
+        echo "invalid"
 fi
